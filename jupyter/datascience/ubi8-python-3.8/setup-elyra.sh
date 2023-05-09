488fc6395ee8c6a6ede96ac75ecdb609647c598c
Harshad Reddy Nalla <hnalla@redhat.com>
2023-05-09 03:41:20 -0400

    Allow elyra pipeline config mount on ~/runtimes dir (#65)
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
+++ jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
@@ -3,3 +3,8 @@ set -x
 
 # Set the elyra config on the right path
 cp /opt/app-root/bin/utils/jupyter_elyra_config.py /opt/app-root/src/.jupyter/
+
+# Set runtime config from volume mount
+if [ "$(ls -A /opt/app-root/runtimes/)" ]; then
+  cp -r /opt/app-root/runtimes/..data/*.json $(jupyter --data-dir)/metadata/runtimes/
+fi
