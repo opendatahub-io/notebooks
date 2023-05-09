4a780ffa77bf46d9a7d9394c1800da74785188fe
Harshad Reddy Nalla <hnalla@redhat.com>
2023-05-09 06:34:27 -0400

    Set default env vars for ssl_sa_certs and sa_token path (#77)
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
+++ jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
@@ -8,3 +8,7 @@ cp /opt/app-root/bin/utils/jupyter_elyra_config.py /opt/app-root/src/.jupyter/
 if [ "$(ls -A /opt/app-root/runtimes/)" ]; then
   cp -r /opt/app-root/runtimes/..data/*.json $(jupyter --data-dir)/metadata/runtimes/
 fi
+
+# Environment vars set for accessing ssl_sa_certs and sa_token
+export KF_PIPELINES_SSL_SA_CERTS="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+export KF_PIPELINES_SA_TOKEN_ENV="/var/run/secrets/kubernetes.io/serviceaccount/token"
