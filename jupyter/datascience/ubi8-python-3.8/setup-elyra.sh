96a92c9511b570bdc7ed8815ecb30a4c2e44eef8
Harshad Reddy Nalla <hnalla@redhat.com>
2023-06-09 12:01:35 -0400

    Include patch option for providing ssl_ca_certs
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
+++ jupyter/datascience/ubi8-python-3.8/setup-elyra.sh
@@ -44,6 +44,6 @@ if [ "$(ls -A /opt/app-root/runtimes/)" ]; then
 fi
 
 # Environment vars set for accessing ssl_sa_certs and sa_token
-export KF_PIPELINES_SSL_SA_CERTS="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+# export PIPELINES_SSL_SA_CERTS="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 export KF_PIPELINES_SA_TOKEN_ENV="/var/run/secrets/kubernetes.io/serviceaccount/token"
 export KF_PIPELINES_SA_TOKEN_PATH="/var/run/secrets/kubernetes.io/serviceaccount/token"
