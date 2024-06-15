7ce69e6a1368f7992f2fde74434a4614f5326c99
Jan Stourac <jstourac@redhat.com>
2024-06-15 20:13:33 +0200

    [CI] check-params-env.sh prints also time of creation of the checked image

--- ci/check-params-env.sh
+++ ci/check-params-env.sh
@@ -248,6 +248,7 @@ function check_image() {
     local image_name
     local image_commit_id
     local image_commitref
+    local image_created
 
     image_metadata="$(skopeo inspect --config "docker://${image_url}")" || {
         echo "Couldn't download image metadata with skopeo tool!"
@@ -263,6 +264,10 @@ function check_image() {
     }
     image_commitref=$(echo "${image_metadata}" | jq --raw-output '.config.Labels."io.openshift.build.commit.ref"') ||  {
         echo "Couldn't parse '.config.Labels."io.openshift.build.commit.ref"' from image metadata!"
+        return 1
+    }
+    image_created=$(echo "${image_metadata}" | jq --raw-output '.created') ||  {
+        echo "Couldn't parse '.created' from image metadata!"
         return 1
     }
 
@@ -289,6 +294,7 @@ function check_image() {
     }
 
     echo "Image name retrieved: '${image_name}'"
+    echo "Image created: '${image_created}'"
 
     check_image_variable_matches_name_and_commitref "${image_variable}" "${image_name}" "${image_commitref}" "${openshift_build_name}" || return 1
 
