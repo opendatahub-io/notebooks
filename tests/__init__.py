cd0116444ee44de0098e22408f30046457c07dd0
Jiri Daněk <jdanek@redhat.com>
2024-07-22 14:02:34 +0200

    fixup, explanatory comments

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,9 @@ import os
 
 import pathlib
 
+# Absolute path to the top level directory
 ROOT_PATH = pathlib.Path(__file__).parent.parent
 
+# Disable Dagger telemetry and PaaS offering
 os.environ["DO_NOT_TRACK"]= "1"
 os.environ["NOTHANKS"]= "1"
