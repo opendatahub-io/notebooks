aaacb974de5617e89cb945892d3801c831b37a44
Jiri Danek <jdanek@redhat.com>
2024-08-02 10:55:53 +0200

    add a selftest

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,3 +20,15 @@ def test_image_pipfiles(subtests: pytest_subtests.plugin.SubTests):
                 pipfile = tomllib.load(fp)
             assert "requires" in pipfile, "Pipfile is missing a [[requires]] section"
             assert pipfile["requires"]["python_version"] == python, "Pipfile does not declare the expected Python version"
+
+
+def test_files_that_should_be_same_are_same(subtests: pytest_subtests.plugin.SubTests):
+    file_groups = {
+        "ROCm de-vendor script":
+            [PROJECT_ROOT / "jupyter/rocm/pytorch/ubi9-python-3.9/de-vendor-torch.sh",
+             PROJECT_ROOT / "runtimes/rocm-pytorch/ubi9-python-3.9/de-vendor-torch.sh"]
+    }
+    for group_name, (first_file, *rest) in file_groups.items():
+        with subtests.test(msg=f"Checking {group_name}"):
+            for file in rest:
+                assert first_file.read_text() == file.read_text(), f"The files {first_file} and {file} do not match"
