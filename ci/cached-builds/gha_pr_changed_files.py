dfb15241ceb92aa58c9cc1de397627b08bbb5bc0
Jiri Daněk <jdanek@redhat.com>
2025-01-17 07:49:18 +0100

    RHOAIENG-8388: rm(intel): Intel tensorflow notebook failed to get tested on OCP-CI
    The Intel workbench images are broken in multiple ways and since we don't ship them, let's not even carry them on the books.

--- ci/cached-builds/gha_pr_changed_files.py
+++ ci/cached-builds/gha_pr_changed_files.py
@@ -88,10 +88,12 @@ class SelfTests(unittest.TestCase):
                                       'codeserver/ubi9-python-3.9/run-code-server.sh'}
 
     def test_analyze_build_directories(self):
-        directories = analyze_build_directories("jupyter-intel-pytorch-ubi9-python-3.11")
+        directories = analyze_build_directories("rocm-jupyter-pytorch-ubi9-python-3.11")
         assert set(directories) == {"base/ubi9-python-3.11",
-                                    "intel/base/gpu/ubi9-python-3.11",
-                                    "jupyter/intel/pytorch/ubi9-python-3.11"}
+                                    "rocm/ubi9-python-3.11",
+                                    "jupyter/minimal/ubi9-python-3.11",
+                                    "jupyter/datascience/ubi9-python-3.11",
+                                    "jupyter/rocm/pytorch/ubi9-python-3.11"}
 
     def test_should_build_target(self):
         assert "" == should_build_target(["README.md"], ["jupyter/datascience/ubi9-python-3.11"])
