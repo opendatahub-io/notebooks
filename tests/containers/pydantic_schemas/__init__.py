5480a2c5c2393c863eeb7411ef6d0cb93ae21009
Jiri Daněk <jdanek@redhat.com>
2025-02-04 17:20:40 +0100

    NO-JIRA: chore(tests/containers): detect socket for ryuk's use with rootless podman on macOS

---
+++ tests/containers/pydantic_schemas/__init__.py
@@ -0,0 +1,8 @@
+from .podman_info import PodmanInfo
+from .podman_machine_inspect import PodmanMachineInspect, PodmanMachine
+
+__all__ = [
+    PodmanInfo,
+    PodmanMachineInspect,
+    PodmanMachine,
+]
