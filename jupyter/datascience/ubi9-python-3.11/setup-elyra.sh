b23bb0c3a1db387ae2489879b012ca729adb7153
atheo89 <atheodorak@outlook.com>
2025-02-20 10:06:22 +0100

    Remove json runtimes from the datascience notebook

--- jupyter/datascience/ubi9-python-3.11/setup-elyra.sh
+++ jupyter/datascience/ubi9-python-3.11/setup-elyra.sh
@@ -14,6 +14,11 @@ if [ "$(ls -A /opt/app-root/runtimes/)" ]; then
   cp -r /opt/app-root/runtimes/..data/*.json $(jupyter --data-dir)/metadata/runtimes/
 fi
 
+# Set elyra runtime images json from volume mount
+if [ "$(ls -A /opt/app-root/pipeline-runtimes/)" ]; then
+  cp -r /opt/app-root/pipeline-runtimes/..data/*.json /opt/app-root/share/jupyter/metadata/runtime-images/
+fi
+
 # Environment vars set for accessing ssl_sa_certs and sa_token
 # export PIPELINES_SSL_SA_CERTS="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 export KF_PIPELINES_SA_TOKEN_ENV="/var/run/secrets/kubernetes.io/serviceaccount/token"
