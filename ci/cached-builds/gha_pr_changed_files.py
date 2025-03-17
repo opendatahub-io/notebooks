a83b4bf8a8d7d5f24c2b1f8890e30d3a11c38ad5
Jiri Daněk <jdanek@redhat.com>
2025-03-17 17:39:26 +0100

    Update ci/cached-builds/gha_pr_changed_files.py
    Co-authored-by: Jan Stourac <jstourac@redhat.com>

--- ci/cached-builds/gha_pr_changed_files.py
+++ ci/cached-builds/gha_pr_changed_files.py
@@ -115,8 +115,8 @@ class SelfTests(unittest.TestCase):
         assert directory == "jupyter/rocm/pytorch/ubi9-python-3.11"
 
     def test_get_build_dockerfile(self):
-        directory = get_build_dockerfile("rocm-jupyter-pytorch-ubi9-python-3.11")
-        assert directory == "jupyter/rocm/pytorch/ubi9-python-3.11/Dockerfile.rocm"
+        dockerfile = get_build_dockerfile("rocm-jupyter-pytorch-ubi9-python-3.11")
+        assert dockerfile == "jupyter/rocm/pytorch/ubi9-python-3.11/Dockerfile.rocm"
 
     def test_should_build_target(self):
         assert "" == should_build_target(["README.md"], "jupyter/datascience/ubi9-python-3.11")
