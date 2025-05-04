1ff4f8be43cf77411a9d254d29372a9b29eaf20b
Jiri Daněk <jdanek@redhat.com>
2025-05-04 22:40:28 +0200

    Update rsession.sh to preserve environment variables
    Modify the rsession.sh script to clone environment variables from the container's root process (PID 1), addressing issues caused by RStudio's purging of variables. This approach was inspired by community discussions and ensures proper variable propagation during R session startup.

--- rstudio/rhel9-python-3.11/rsession.sh
+++ rstudio/rhel9-python-3.11/rsession.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Copy environment variables from PID 1, the container's root process
+# This is to undo RStudio's purging of environment variables prior to launching rsession
+# The idea of using `--rsession-path=rsession.sh` was suggested at
+#  https://github.com/jupyterhub/jupyter-rsession-proxy/issues/135
+# The command to clone (startup) environment variables from another process using Bash comes from
+#  https://unix.stackexchange.com/questions/125110/how-do-i-source-another-processs-environment-variables
+source <(xargs -0 bash -c 'printf "export %q\n" "$@"' -- < /proc/1/environ)
+
 /usr/lib/rstudio-server/bin/rsession "$@"
