4bd800b07f8d45c101689f7cb9f035d62c61f371
William Siqueira <william.fatecsjc@gmail.com>
2025-05-07 11:58:19 -0300

    Review changes

--- jupyter/utils/install_tex_live.sh
+++ jupyter/utils/install_tex_live.sh
@@ -1,7 +1,8 @@
-echo "Installing TexLive to allow PDf export from Notebooks" && \
-curl -L https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz && \
-zcat < install-tl-unx.tar.gz | tar xf - && \
-cd install-tl-2* && \
-perl ./install-tl --no-interaction --scheme=scheme-small && \
-cd /usr/local/texlive/2025/bin/x86_64-linux && \
+#!/bin/bash 
+echo "Installing TexLive to allow PDf export from Notebooks" 
+curl -L https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz 
+zcat < install-tl-unx.tar.gz | tar xf -
+cd install-tl-2*
+perl ./install-tl --no-interaction --scheme=scheme-small
+cd /usr/local/texlive/2025/bin/x86_64-linux
 ./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
