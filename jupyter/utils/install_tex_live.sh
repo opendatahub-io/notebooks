294de99db0aae17ad460d2e9984795d116efc80f
William Siqueira <william.fatecsjc@gmail.com>
2025-05-15 14:57:13 -0300

    Removing year from the TeXLive installation

--- jupyter/utils/install_tex_live.sh
+++ jupyter/utils/install_tex_live.sh
@@ -3,6 +3,6 @@ echo "Installing TexLive to allow PDf export from Notebooks"
 curl -L https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz 
 zcat < install-tl-unx.tar.gz | tar xf -
 cd install-tl-2*
-perl ./install-tl --no-interaction --scheme=scheme-small
-cd /usr/local/texlive/2025/bin/x86_64-linux
+perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
+cd /usr/local/texlive/bin/x86_64-linux
 ./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
