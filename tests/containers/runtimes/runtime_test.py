c8200765cfae049a3a72a52085bbed2e6a980e59
Jiri Daněk <jdanek@redhat.com>
2025-06-19 18:40:38 +0200

    RHOAIENG-26279: remove unused WorkbenchContainer import and cleanup test logic

--- tests/containers/runtimes/runtime_test.py
+++ tests/containers/runtimes/runtime_test.py
@@ -7,7 +7,6 @@ import pytest
 import testcontainers.core.container
 
 from tests.containers import base_image_test, conftest, docker_utils
-from tests.containers.workbenches.workbench_image_test import WorkbenchContainer
 
 
 class TestRuntimeImage:
@@ -15,8 +14,6 @@ class TestRuntimeImage:
 
     @allure.description("Check that pyzmq library works correctly, important to check especially on s390x.")
     def test_pyzmq_import(self, runtime_image: conftest.Image) -> None:
-        container = WorkbenchContainer(image=runtime_image.name, user=4321, group_add=[0])
-
         def check_zmq():
             # ruff: noqa: PLC0415 `import` should be at the top-level of a file
             import zmq  # pyright: ignore reportMissingImports
@@ -37,10 +34,10 @@ class TestRuntimeImage:
                 base_image_test.encode_python_function_execution_command_interpreter("python3", check_zmq)
             )
 
-            assert exit_code == 0, f"Python script execution failed. Output: {output_str}"
-            assert "pyzmq imported and socket created successfully" in output_str, (
-                f"Expected success message not found in output. Output: {output_str}"
-            )
+        assert exit_code == 0, f"Python script execution failed. Output: {output_str}"
+        assert "pyzmq imported and socket created successfully" in output_str, (
+            f"Expected success message not found in output. Output: {output_str}"
+        )
 
 
 @contextlib.contextmanager
