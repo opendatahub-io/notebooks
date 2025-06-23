395362d6537754f16143b63be43776615f76da4c
puneetsharma21 <puneet.sharma21@ibm.com>
2025-06-23 06:20:59 -0400

    Add TeX Live install for ppc64le
    # Conflicts:
    #	jupyter/utils/install_pdf_deps.sh

--- jupyter/utils/install_pdf_deps.sh
+++ jupyter/utils/install_pdf_deps.sh
@@ -23,15 +23,46 @@ if [[ "$(uname -m)" == "s390x" ]]; then
     exit 0
 fi
 
-# tex live installation
-echo "Installing TexLive to allow PDf export from Notebooks"
-curl -fL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
-zcat < install-tl-unx.tar.gz | tar xf -
-cd install-tl-2*
-perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
-mv /usr/local/texlive/bin/"$(uname -m)-linux" /usr/local/texlive/bin/linux
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "ppc64le" ]]; then
+  echo "Installing TeX Live from source for $ARCH"
+
+  # Download and extract source
+  wget https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2025/texlive-20250308-source.tar.xz
+  tar -xf texlive-20250308-source.tar.xz
+  cd texlive-20250308-source
+
+  # Install build dependencies
+  dnf install -y gcc gcc-c++ perl make libX11-devel libXt-devel \
+    zlib-devel freetype-devel libpng-devel ncurses-devel \
+    gd-devel libtool wget tar xz bison flex libXaw-devel
+
+  # Create build directory
+  mkdir ../texlive-build
+  cd ../texlive-build
+
+  # Configure, build, install
+  ../texlive-20250308-source/configure --prefix=/opt/texlive/2025
+  make -j$(nproc)
+  make install
+
+  # Symlink for pdflatex
+  cd /opt/texlive/2025/bin/powerpc64le-unknown-linux-gnu
+  ln -s pdftex pdflatex
+
+else
+  echo "Installing TeX Live from installer for $ARCH"
+
+  # Download and install from official installer
+  curl -fL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
+  zcat < install-tl-unx.tar.gz | tar xf -
+  cd install-tl-2*
+  perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
+  mv /usr/local/texlive/bin/"$(uname -m)-linux" /usr/local/texlive/bin/linux
 cd /usr/local/texlive/bin/linux
-./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
+  ./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
+fi
 
 # pandoc installation
 curl -fL "https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-${ARCH}.tar.gz"  -o /tmp/pandoc.tar.gz
