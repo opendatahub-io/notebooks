d6ec586caa77baea1eae6c78bd53d798c8b833bd
Gerard Ryan <git@grdryn.xyz>
2025-07-02 15:42:15 +0100

    Allow install_pdf_deps.sh to work for multiarch
    This will allow Dockerfiles that use this script to at least get the
    correct texlive and pandoc for arm64 along with the existing amd64.
    
    The corresponding change to Dockerfiles that use it here, are because
    of the hardcoded x86_64 in the binary path: the script now renames
    that directory to be more generic, so the references to it must all be
    changed.

--- jupyter/utils/install_pdf_deps.sh
+++ jupyter/utils/install_pdf_deps.sh
@@ -1,20 +1,34 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Install dependencies required for Notebooks PDF exports
 
-set -euxo
+set -euxo pipefail
+
+# Mapping of `uname -m` values to equivalent GOARCH values
+declare -A UNAME_TO_GOARCH
+UNAME_TO_GOARCH["x86_64"]="amd64"
+UNAME_TO_GOARCH["aarch64"]="arm64"
+UNAME_TO_GOARCH["ppc64le"]="ppc64le"
+UNAME_TO_GOARCH["s390x"]="s390x"
+
+ARCH="${UNAME_TO_GOARCH[$(uname -m)]}"
+if [[ -z "${ARCH:-}" ]]; then
+    echo "Unsupported architecture: $(uname -m)" >&2
+    exit 1
+fi
 
 # tex live installation
-echo "Installing TexLive to allow PDf export from Notebooks" 
-curl -L https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz 
+echo "Installing TexLive to allow PDf export from Notebooks"
+curl -fL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
 zcat < install-tl-unx.tar.gz | tar xf -
 cd install-tl-2*
 perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
-cd /usr/local/texlive/bin/x86_64-linux
+mv /usr/local/texlive/bin/"$(uname -m)-linux" /usr/local/texlive/bin/linux
+cd /usr/local/texlive/bin/linux
 ./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
 
 # pandoc installation
-curl -L https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-amd64.tar.gz  -o /tmp/pandoc.tar.gz
+curl -fL "https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-${ARCH}.tar.gz"  -o /tmp/pandoc.tar.gz
 mkdir -p /usr/local/pandoc
 tar xvzf /tmp/pandoc.tar.gz --strip-components 1 -C /usr/local/pandoc/
 rm -f /tmp/pandoc.tar.gz
