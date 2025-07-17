1ef509e987eea264bc2808ceb7062e9660c3b9e4
Jiri Daněk <jdanek@redhat.com>
2025-07-17 18:15:22 +0200

    upd

--- tests/containers/workbenches/jupyterlab/jupyterlab_datascience_test.py
+++ tests/containers/workbenches/jupyterlab/jupyterlab_datascience_test.py
@@ -158,9 +158,7 @@ except Exception as e:
 
             # RHOAIENG-140: code-server image users are expected to install their own db clients
             if "-code-server-" in datascience_image.labels["name"]:
-                exit_code, output = container.exec(
-                    ["python", "-m", "pip", "install", "mysql-connector-python==9.3.0"]
-                )
+                exit_code, output = container.exec(["python", "-m", "pip", "install", "mysql-connector-python==9.3.0"])
                 output_str = output.decode()
                 print(output_str)
 
