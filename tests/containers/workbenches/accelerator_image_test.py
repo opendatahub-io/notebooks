bacf5679d15b811ab0fa89d31007224a49083550
Jiri Daněk <jdanek@redhat.com>
2025-07-18 10:43:16 +0200

    test(tests/containers): add test for ROCm accelerator images (#1414)
    This commit introduces tests for ROCm-enabled workbench images on OpenShift. These tests verify that the images can be deployed successfully on a cluster with AMD GPUs and that both PyTorch and TensorFlow can correctly detect the available accelerator.
    To support the testing of large accelerator images, the following changes were made:
    - The pod readiness timeout in the test framework has been increased to 10 minutes to allow sufficient time for image pulling.
    - The utility was updated to allow for configurable timeouts. `ImageDeployment`
    - Existing CUDA tests were updated to use this new configurable timeout.

--- tests/containers/workbenches/accelerator_image_test.py
+++ tests/containers/workbenches/accelerator_image_test.py
@@ -5,6 +5,7 @@ import shlex
 import pytest
 
 from tests.containers import conftest, kubernetes_utils
+from tests.containers.kubernetes_utils import TestFrameConstants
 
 code = """
 import torch; device = "cuda" if torch.cuda.is_available() else "cpu"; print(f"Using {device} device")
@@ -16,12 +17,12 @@ import torch; device = "cuda" if torch.cuda.is_available() else "cpu"; print(f"U
 class TestAccelerator:
     @pytest.mark.cuda
     @pytest.mark.openshift
-    # image must be both jupyterlab image and cuda workbench image
-    def test_cuda_run_on_openshift(self, jupyterlab_image, cuda_workbench_image):
+    # image must be both a datascience image and cuda image
+    def test_cuda_run_on_openshift(self, datascience_image, cuda_image):
         client = kubernetes_utils.get_client()
         print(client)
 
-        image_metadata = conftest.get_image_metadata(cuda_workbench_image)
+        image_metadata = conftest.get_image_metadata(cuda_image)
         library = None
         if "-pytorch-" in image_metadata.labels.get("name"):
             library = "torch"
@@ -35,8 +36,52 @@ class TestAccelerator:
         # language=python
         tensorflow_check = """import tensorflow as tf; import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'; print(tf.config.list_physical_devices('GPU'))"""
 
-        with kubernetes_utils.ImageDeployment(client, cuda_workbench_image) as image:
-            image.deploy(container_name="notebook-tests-pod", accelerator="nvidia.com/gpu")
+        with kubernetes_utils.ImageDeployment(client, cuda_image) as image:
+            image.deploy(
+                container_name="notebook-tests-pod",
+                accelerator="nvidia.com/gpu",
+                is_runtime_image="-runtime-" in image_metadata.labels.get("name"),
+                timeout=TestFrameConstants.TIMEOUT_20MIN,
+            )
+            if library == "torch":
+                result = image.exec(shlex.join(["python", "-c", torch_check]))
+                assert "Using cuda device" in result.stdout
+            elif library == "tensorflow":
+                result = image.exec(shlex.join(["python", "-c", tensorflow_check]))
+                assert "[PhysicalDevice(name='/physical_device:GPU:0', device_type='GPU')]" in result.stdout
+            else:
+                raise ValueError(f"Unknown library {library}")
+
+    @pytest.mark.rocm
+    @pytest.mark.openshift
+    # image must be both a datascience image and rocm image
+    def test_rocm_run_on_openshift(self, datascience_image, rocm_image):
+        client = kubernetes_utils.get_client()
+        print(client)
+
+        image_metadata = conftest.get_image_metadata(rocm_image)
+        library = None
+        if "-pytorch-" in image_metadata.labels.get("name"):
+            library = "torch"
+        if "-tensorflow-" in image_metadata.labels.get("name"):
+            library = "tensorflow"
+
+        # NOTE: the basic check is exactly the same as for cuda; in torch, even though it says "cuda", it is actually ROCm
+
+        # language=python
+        torch_check = (
+            """import torch; device = "cuda" if torch.cuda.is_available() else "cpu"; print(f"Using {device} device")"""
+        )
+        # language=python
+        tensorflow_check = """import tensorflow as tf; import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'; print(tf.config.list_physical_devices('GPU'))"""
+
+        with kubernetes_utils.ImageDeployment(client, rocm_image) as image:
+            image.deploy(
+                container_name="notebook-tests-pod",
+                accelerator="amd.com/gpu",
+                is_runtime_image="-runtime-" in image_metadata.labels.get("name"),
+                timeout=TestFrameConstants.TIMEOUT_20MIN,
+            )
             if library == "torch":
                 result = image.exec(shlex.join(["python", "-c", torch_check]))
                 assert "Using cuda device" in result.stdout
