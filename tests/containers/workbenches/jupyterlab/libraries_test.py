8769a75e3a5791678e9c144a9a18c76c673e4120
Jiri Daněk <jdanek@redhat.com>
2025-07-21 10:28:44 +0200

    tests(containers): add unittests for verifying data science libraries in workbench images**
    Introduces a new test suite (`libraries_testunits.py`) to validate key data science libraries (NumPy, Pandas, scikit-learn, Matplotlib, PyTorch, TorchVision, and TorchAudio) within workbench container images. Adds integration of these tests in `libraries_test.py`.
    
    Extends `docker_utils.container_cp` to accept `PathLike` types.

--- tests/containers/workbenches/jupyterlab/libraries_test.py
+++ tests/containers/workbenches/jupyterlab/libraries_test.py
@@ -4,7 +4,7 @@ import pathlib
 from typing import TYPE_CHECKING
 
 from tests.containers import docker_utils
-from tests.containers.conftest import get_image_metadata
+from tests.containers.conftest import get_image_metadata, Image
 from tests.containers.workbenches.workbench_image_test import grab_and_check_logs, WorkbenchContainer
 
 if TYPE_CHECKING:
@@ -15,8 +15,8 @@ class TestWorkbenchImage:
     """Tests for workbench images in this repository.
     A workbench image is an image running a web IDE that listens on port 8888."""
 
-    def test_image_entrypoint_starts(self, subtests: pytest_subtests.SubTests, workbench_image: str) -> None:
-        container = WorkbenchContainer(image=workbench_image, user=1000, group_add=[0])
+    def test_image_entrypoint_starts(self, subtests: pytest_subtests.SubTests, jupyterlab_datascience_image: Image) -> None:
+        container = WorkbenchContainer(image=jupyterlab_datascience_image.name, user=1000, group_add=[0])
         try:
             try:
                 container.start()
@@ -26,9 +26,8 @@ class TestWorkbenchImage:
                 unittests = pathlib.Path(__file__).parent / "libraries_testunits.py"
                 docker_utils.container_cp(container.get_wrapped_container(), unittests,
                                           "/opt/app-root/src/")
-                image_metadata = get_image_metadata(workbench_image)
                 ecode, stdout = container.exec([
-                    "env", f"IMAGE={image_metadata.labels["name"]}",
+                    "env", f"IMAGE={jupyterlab_datascience_image.labels["name"]}",
                     "bash", "-c", "python3 /opt/app-root/src/libraries_testunits.py"
                 ])
                 stdout_decoded = stdout.decode()
