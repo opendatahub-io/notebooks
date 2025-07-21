d257f9bfbfd0baa65895a3f8bd9fdfb865e44d33
Jiri Daněk <jdanek@redhat.com>
2025-07-21 10:16:46 +0200

    **tests(containers): add unittests for verifying data science libraries in workbench images**
    Introduces a new test suite (`libraries_testunits.py`) to validate key data science libraries (NumPy, Pandas, scikit-learn, Matplotlib, PyTorch, TorchVision, and TorchAudio) within workbench container images. Adds integration of these tests in `libraries_test.py`.
    
    Extends `docker_utils.container_cp` to accept `PathLike` types.

---
+++ tests/containers/workbenches/libraries_test.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import pathlib
+from typing import TYPE_CHECKING
+
+from tests.containers import docker_utils
+from tests.containers.conftest import get_image_metadata
+from tests.containers.workbenches.workbench_image_test import grab_and_check_logs, WorkbenchContainer
+
+if TYPE_CHECKING:
+    import pytest_subtests
+
+
+class TestWorkbenchImage:
+    """Tests for workbench images in this repository.
+    A workbench image is an image running a web IDE that listens on port 8888."""
+
+    def test_image_entrypoint_starts(self, subtests: pytest_subtests.SubTests, workbench_image: str) -> None:
+        container = WorkbenchContainer(image=workbench_image, user=1000, group_add=[0])
+        try:
+            try:
+                container.start()
+                # check explicitly that we can connect to the ide running in the workbench
+                with subtests.test("Attempting to connect to the workbench..."):
+                    container._connect()
+                unittests = pathlib.Path(__file__).parent / "libraries_testunits.py"
+                docker_utils.container_cp(container.get_wrapped_container(), unittests,
+                                          "/opt/app-root/src/")
+                image_metadata = get_image_metadata(workbench_image)
+                ecode, stdout = container.exec([
+                    "env", f"IMAGE={image_metadata.labels["name"]}",
+                    "bash", "-c", "python3 /opt/app-root/src/libraries_testunits.py"
+                ])
+                stdout_decoded = stdout.decode()
+                print(stdout_decoded)
+                assert ecode == 0, stdout_decoded
+            finally:
+                # try to grab logs regardless of whether container started or not
+                grab_and_check_logs(subtests, container)
+        finally:
+            docker_utils.NotebookContainer(container).stop(timeout=0)
