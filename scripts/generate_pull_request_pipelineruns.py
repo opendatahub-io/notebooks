acc75ab0a01b76825249bca6236f131430aa3cb7
Jiri Daněk <jdanek@redhat.com>
2025-09-05 10:07:10 +0200

    enable ppc64le in github actions and in konflux, subject to further review
    (cherry picked from commit 42493f4b8bbe25a92c08bfd7b74797fb62db2404)
    * https://github.com/opendatahub-io/notebooks/pull/2286/commits

--- scripts/generate_pull_request_pipelineruns.py
+++ scripts/generate_pull_request_pipelineruns.py
@@ -143,6 +143,7 @@ def transform_build_pipeline_to_pr_pipeline(push_pipeline_path: pathlib.Path):
 
     if component in [
         "odh-workbench-codeserver-datascience-cpu-py312-ubi9",
+        "odh-workbench-jupyter-datascience-cpu-py312-ubi9",
         "odh-workbench-jupyter-minimal-cpu-py312-ubi9",
         "odh-pipeline-runtime-minimal-cpu-py312-ubi9",
         "odh-pipeline-runtime-datascience-cpu-py312-ubi9",
