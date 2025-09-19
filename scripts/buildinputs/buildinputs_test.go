d9d8cb70982be29f44f235c731fa0c626681fb2a
Jiri Daněk <jdanek@redhat.com>
2025-09-19 17:58:44 +0200

    Issue #2521: fix(scripts/buildinputs): fix build input detection to deal with mounts (#2522)
    ```
    + /Users/jdanek/IdeaProjects/notebooks/scripts/sandbox.py --dockerfile codeserver/ubi9-python-3.12/Dockerfile.cpu --platform linux/amd64 -- podman build --no-cache --platform=linux/amd64 --label release=2025a --tag quay.io/opendatahub/workbench-images:codeserver-ubi9-python-3.12-2025a_20250919 --file codeserver/ubi9-python-3.12/Dockerfile.cpu --build-arg BASE_IMAGE=registry.access.redhat.com/ubi9/python-312:latest '{};'
    __file__='/Users/jdanek/IdeaProjects/notebooks/scripts/sandbox.py' started with args=Namespace(dockerfile=PosixPath('codeserver/ubi9-python-3.12/Dockerfile.cpu'), platform='linux/amd64', remaining=['--', 'podman', 'build', '--no-cache', '--platform=linux/amd64', '--label', 'release=2025a', '--tag', 'quay.io/opendatahub/workbench-images:codeserver-ubi9-python-3.12-2025a_20250919', '--file', 'codeserver/ubi9-python-3.12/Dockerfile.cpu', '--build-arg', 'BASE_IMAGE=registry.access.redhat.com/ubi9/python-312:latest', '{};'])
    prereqs=[PosixPath('codeserver/ubi9-python-3.12/test'), PosixPath('foo'), PosixPath('tmp/test_log.txt')]
    Traceback (most recent call last):
      File "/Users/jdanek/IdeaProjects/notebooks/scripts/sandbox.py", line 91, in <module>
        sys.exit(main())
                 ^^^^^^
      File "/Users/jdanek/IdeaProjects/notebooks/scripts/sandbox.py", line 49, in main
        setup_sandbox(prereqs, pathlib.Path(tmpdir))
      File "/Users/jdanek/IdeaProjects/notebooks/scripts/sandbox.py", line 87, in setup_sandbox
        shutil.copy(dep, tmpdir / dep.parent)
      File "/opt/homebrew/Cellar/python@3.12/3.12.11/Frameworks/Python.framework/Versions/3.12/lib/python3.12/shutil.py", line 435, in copy
        copyfile(src, dst, follow_symlinks=follow_symlinks)
      File "/opt/homebrew/Cellar/python@3.12/3.12.11/Frameworks/Python.framework/Versions/3.12/lib/python3.12/shutil.py", line 260, in copyfile
        with open(src, 'rb') as fsrc:
             ^^^^^^^^^^^^^^^
    FileNotFoundError: [Errno 2] No such file or directory: 'foo'
    gmake: *** [Makefile:177: codeserver-ubi9-python-3.12] Error 1
    ```

--- scripts/buildinputs/buildinputs_test.go
+++ scripts/buildinputs/buildinputs_test.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"os"
 	"path/filepath"
 	"reflect"
 	"runtime"
+	"slices"
 	"strings"
 	"testing"
 )
@@ -40,19 +40,75 @@ func TestParseAllDockerfiles(t *testing.T) {
 	for _, dockerfile := range dockerfiles {
 		t.Run(dockerfile, func(t *testing.T) {
 			result := getDockerfileDeps(dockerfile, "amd64")
-			if result == "" {
+			if len(result) == 0 {
 				// no deps in the dockerfile
 				return
 			}
-			data := make([]string, 0)
-			noErr(json.Unmarshal([]byte(result), &data))
-			for _, path := range data {
-				stat := noErr2(os.Stat(filepath.Join(projectRoot, path)))
+			for _, path := range result {
+				stat, err := os.Stat(filepath.Join(projectRoot, path))
+				if err != nil {
+					t.Fatal(err)
+				}
 				if stat.IsDir() {
 					// log this very interesting observation
 					t.Logf("dockerfile copies in a whole directory: %s", path)
 				}
 			}
 		})
+	}
+}
+
+// TestParseDockerfileWithBindMount checks for a bug where a Dockerfile with RUN --mount=type=bind,src=foo,dst=bar would report it has no inputs
+func TestParseDockerfileWithBindMount(t *testing.T) {
+	dockerfile := filepath.Join(t.TempDir(), "Dockerfile")
+	// language=Dockerfile
+	noErr(os.WriteFile(dockerfile, []byte(Doc(`
+		FROM codeserver AS tests
+		ARG CODESERVER_SOURCE_CODE=codeserver/ubi9-python-3.12
+		COPY ${CODESERVER_SOURCE_CODE}/test /tmp/test
+		RUN --mount=type=tmpfs,target=/opt/app-root/src --mount=type=bind,src=foo,dst=bar <<'EOF'
+		set -Eeuxo pipefail
+		python3 /tmp/test/test_startup.py |& tee /tmp/test_log.txt
+		EOF
+	`)), 0644))
+
+	//dockerfile = "/Users/jdanek/IdeaProjects/notebooks/jupyter/rocm/pytorch/ubi9-python-3.12/Dockerfile.rocm"
+
+	result := getDockerfileDeps(dockerfile, "amd64")
+	expected := []string{"codeserver/ubi9-python-3.12/test", "foo"}
+	if !reflect.DeepEqual(
+		slices.Sorted(slices.Values(result)),
+		slices.Sorted(slices.Values(expected)),
+	) {
+		t.Errorf("expected %v but got %v", expected, result)
+	}
+}
+
+func TestParseFileWithStageCopy(t *testing.T) {
+	dockerfile := filepath.Join(t.TempDir(), "Dockerfile")
+	// language=Dockerfile
+	noErr(os.WriteFile(dockerfile, []byte(Doc(`
+		FROM codeserver
+		COPY --from=registry.access.redhat.com/ubi9/ubi /etc/yum.repos.d/ubi.repo /etc/yum.repos.d/ubi.repo
+	`)), 0644))
+
+	result := getDockerfileDeps(dockerfile, "amd64")
+	if len(result) != 0 {
+		t.Fatalf("unexpected deps reported for the dockerfile: %s", result)
+	}
+}
+
+func TestParseFileWithStageMount(t *testing.T) {
+	dockerfile := filepath.Join(t.TempDir(), "Dockerfile")
+	// language=Dockerfile
+	noErr(os.WriteFile(dockerfile, []byte(Doc(`
+		FROM javabuilder
+		RUN --mount=type=bind,from=build,source=/.m2_repository,target=/.m2_repository \
+			mvn package
+	`)), 0644))
+
+	result := getDockerfileDeps(dockerfile, "amd64")
+	if len(result) != 0 {
+		t.Fatalf("unexpected deps reported for the dockerfile: %s", result)
 	}
 }
