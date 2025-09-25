2960284a1b9d12849b9a2a73c9723399e9c4a3c3
atheo89 <atheodorak@outlook.com>
2025-09-25 11:56:50 +0200

    Upgrade RStudio c9s

---
+++ rstudio/c9s-python-3.12/nginx/root/usr/share/container-scripts/nginx/common.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# get_matched_files finds file for image extending
+function get_matched_files() {
+  local custom_dir default_dir
+  custom_dir="$1"
+  default_dir="$2"
+  files_matched="$3"
+  find "$default_dir" -maxdepth 1 -type f -name "$files_matched" -printf "%f\n"
+  [ -d "$custom_dir" ] && find "$custom_dir" -maxdepth 1 -type f -name "$files_matched" -printf "%f\n"
+}
+
+# process_extending_files process extending files in $1 and $2 directories
+# - source all *.sh files
+#   (if there are files with same name source only file from $1)
+function process_extending_files() {
+  local custom_dir default_dir
+  custom_dir=$1
+  default_dir=$2
+  while read -r filename ; do
+    if [ "$filename" ]; then
+      echo "=> sourcing $filename ..."
+      # custom file has precedence
+      if [ -f "$custom_dir/$filename" ]; then
+        source "$custom_dir/$filename"
+      elif [ -f "$default_dir/$filename" ]; then
+        source "$default_dir/$filename"
+      fi
+    fi
+  done <<<"$(get_matched_files "$custom_dir" "$default_dir" '*.sh' | sort -u)"
+}
