3df7965eb054dae47173260a4ba910ad218effd3
Daniel Lutz <github227@daniellutz.com.br>
2025-10-08 09:30:08 -0300

    Update ROCm PyTorch version tests
    Co-authored-by: Jiri Daněk <jdanek@redhat.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -279,7 +279,7 @@ def test_image_pyprojects_version_alignment(subtests: pytest_subtests.plugin.Sub
         ("wheel", ("==0.45.1", "~=0.45.1")),
         ("tensorboard", ("~=2.18.0", "~=2.20.0")),
         ("torch", ("==2.7.1", "==2.7.1+cu128", "==2.7.1+rocm6.2.4", "==2.7.1+rocm6.3")),
-        ("torchvision", ("==0.22.1", "~=0.22.1", "==0.22.1+cu128", "==0.22.1+rocm6.2.4", "==0.22.1+rocm6.3")),
+        ("torchvision", ("==0.22.1", "~=0.22.1", "==0.22.1+cu128", "==0.22.1+rocm6.3")),
         (
             "matplotlib",
             ("~=3.10.6",),
