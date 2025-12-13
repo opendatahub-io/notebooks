50908bbc19298aa6f44e41b42903a0dfb4292a2f
Vath. Sok <4911597+ysok@users.noreply.github.com>
2025-12-13 13:49:45 -0500

    Fixed few error before update codeflare-sdk

--- codeserver/ubi9-python-3.12/get_code_server_rpm.sh
+++ codeserver/ubi9-python-3.12/get_code_server_rpm.sh
@@ -31,7 +31,7 @@ if [[ "$ARCH" == "amd64" || "$ARCH" == "arm64" || "$ARCH" == "ppc64le" || "$ARCH
 	# install build dependencies
 	# https://access.redhat.com/support/policy/updates/rhel-app-streams-life-cycle
 	# https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/9/html/developing_c_and_cpp_applications_in_rhel_9/assembly_additional-toolsets-for-development-rhel-9_developing-applications#cpp-compatibility-in-gcc-toolset-14_gcc-toolset-14
-	dnf install -y jq patch libtool rsync gettext gcc-toolset-14 krb5-devel libX11-devel
+	dnf install -y jq patch libtool rsync gettext gcc-toolset-14 gcc-toolset-14-libatomic-devel krb5-devel libX11-devel
 
 	# starting with node-22, c++20 is required
 	. /opt/rh/gcc-toolset-14/enable
