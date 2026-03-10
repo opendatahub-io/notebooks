7fc7243ff74e05e4eb27ec07c2822507802ff06c
atheo89 <atheodorak@outlook.com>
2026-03-10 09:32:31 +0100

    Fix trusty test by using relative path instead of url

--- scripts/test_jupyter_with_papermill.sh
+++ scripts/test_jupyter_with_papermill.sh
@@ -267,7 +267,7 @@ function _run_test()
     local output_file_prefix=
     output_file_prefix=$(tr '/' '-' <<< "${notebook_id}_${os_flavor}")
 
-    "${kbin}" cp "${repo_test_directory}/${test_notebook_file}" "${notebook_workload_name}:./${test_notebook_file}"
+    "${kbin}" cp "${repo_test_directory}/." "${notebook_workload_name}:./"
 
 	if ! "${kbin}" exec "${notebook_workload_name}" -- /bin/sh -c "export IPY_KERNEL_LOG_LEVEL=DEBUG; python3 -m papermill ${test_notebook_file} ${output_file_prefix}_output.ipynb --kernel python3 --log-level DEBUG --stderr-file ${output_file_prefix}_error.txt" ; then
 		echo "ERROR: The ${notebook_id} ${os_flavor} notebook encountered a failure. To investigate the issue, you can review the logs located in the ocp-ci cluster on 'artifacts/notebooks-e2e-tests/jupyter-${notebook_id}-${os_flavor}-${python_flavor}-test-e2e' directory or run 'cat ${output_file_prefix}_error.txt' within your container. The make process has been aborted."
