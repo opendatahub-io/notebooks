0b2e496600df9925231bb69f39de3201b697fa80
Hannah Tosi <htosi@redhat.com>
2026-06-09 09:26:59 -0400

    fix setup kale script
    Signed-off-by: Hannah Tosi <htosi@redhat.com>

--- jupyter/minimal/ubi9-python-3.12/start-notebook.sh
+++ jupyter/minimal/ubi9-python-3.12/start-notebook.sh
@@ -16,24 +16,24 @@ fi
 NOTEBOOK_PROGRAM_ARGS=""
 
 # Set default ServerApp.port value if NOTEBOOK_PORT variable is defined
-if [ -n "${NOTEBOOK_PORT}" ]; then
+if [ -n "${NOTEBOOK_PORT:-}" ]; then
     NOTEBOOK_PROGRAM_ARGS+="--ServerApp.port=${NOTEBOOK_PORT} "
 fi
 
 # Set default ServerApp.base_url value if NOTEBOOK_BASE_URL variable is defined
-if [ -n "${NOTEBOOK_BASE_URL}" ]; then
+if [ -n "${NOTEBOOK_BASE_URL:-}" ]; then
     NOTEBOOK_PROGRAM_ARGS+="--ServerApp.base_url=${NOTEBOOK_BASE_URL} "
 fi
 
 # Set default ServerApp.root_dir value if NOTEBOOK_ROOT_DIR variable is defined
-if [ -n "${NOTEBOOK_ROOT_DIR}" ]; then
+if [ -n "${NOTEBOOK_ROOT_DIR:-}" ]; then
     NOTEBOOK_PROGRAM_ARGS+="--ServerApp.root_dir=${NOTEBOOK_ROOT_DIR} "
 else
     NOTEBOOK_PROGRAM_ARGS+="--ServerApp.root_dir=${HOME} "
 fi
 
 # Add additional arguments if NOTEBOOK_ARGS variable is defined
-if [ -n "${NOTEBOOK_ARGS}" ]; then
+if [ -n "${NOTEBOOK_ARGS:-}" ]; then
     NOTEBOOK_PROGRAM_ARGS+=${NOTEBOOK_ARGS}
 fi
 
